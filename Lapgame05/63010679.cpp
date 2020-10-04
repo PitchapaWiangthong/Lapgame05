176b2d15c6839e94218dc7fc402b5f8156509c42
perth pp <kekskcfm@gmail.com>
2020-10-04 20:17:51 +0700

    Add project files.

---
+++ Lapgame05/63010679.cpp
@@ -0,0 +1,114 @@
+#include<stdio.h>
+#include<conio.h>
+#include<windows.h>
+
+void draw_ship(int, int);
+void erase_ship(int, int);
+void gotoxy(int, int);
+void setcursor(bool);
+void setcolor(int, int);
+void draw_bullet(int, int);
+void erase_bullet(int, int);
+
+
+int main()
+{
+	char ch = '.';
+	char direction = 's';
+	int x = 38, y = 20,bx[5],by[5];
+	bool state[5] = {0};
+	setcursor(0);
+	gotoxy(x,y);
+	draw_ship(x,y);
+	do {
+		if (_kbhit()) {
+			ch = _getch();
+			if (ch == 'a')	{direction = 'l';}
+			if (ch == 'd')	{direction = 'r';}
+			if (ch == 's')	{direction = 's';}
+			if (ch == ' ') {
+				for (int i = 0; i < 5; i++)
+				{
+					if (state[i] == 0)
+					{
+						state[i] = 1;
+						bx[i] = x + 2;
+						by[i] = y - 1;
+						break;
+					}
+				}
+			}
+			fflush(stdin);
+		}
+		if (direction == 'l' && x != 0) { erase_ship(x, y); draw_ship(--x, y); }
+		if (direction == 'r' && x <= 80) { erase_ship(x, y); draw_ship(++x, y); }
+		if (direction == 's') { erase_ship(x, y); draw_ship(x, y); }
+		for (int i = 0; i < 5; i++)
+		{
+			if (state[i] == 1)
+			{
+				erase_bullet(bx[i], by[i]);
+				if (by[i] > 0)
+				{
+					draw_bullet(bx[i], --by[i]);
+				}
+				else
+				{
+					state[i] = 0;
+				}
+			}
+		}
+		Sleep(100);
+	} while (ch != 'p');
+	return 0;
+}
+
+
+
+
+
+void draw_ship(int x,int y)
+{	
+	gotoxy(x,y);
+	setcolor(2, 4);
+	printf("<-0->");
+}
+
+void erase_ship(int x,int y)
+{
+	gotoxy(x,y);
+	setcolor(2, 0);
+	printf("       ");
+}
+
+void gotoxy(int x, int y)
+{
+	COORD c = { x, y };
+	SetConsoleCursorPosition(
+		GetStdHandle(STD_OUTPUT_HANDLE), c);
+}
+void setcursor(bool visible)
+{
+	HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_CURSOR_INFO lpCursor;
+	lpCursor.bVisible = visible;
+	lpCursor.dwSize = 20;
+	SetConsoleCursorInfo(console, &lpCursor);
+}
+void setcolor(int fg, int bg)
+{
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(hConsole, bg * 16 + fg);
+}
+void draw_bullet(int x, int y)
+{	
+	gotoxy(x, y);
+	setcolor(4, 4);
+	printf(" ");
+}
+void erase_bullet(int x, int y)
+{
+	gotoxy(x, y);
+	setcolor(0, 0);
+	printf(" ");
+}
